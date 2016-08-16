d611c48d2d0dcc507d0f7370c91eb7b6866b527f
William <wmmclarke@gmail.com>
2016-08-16 09:16:57 +0100

    Karabiner import settings

---
+++ applications/karabiner/link.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+mv /Users/wmmc/Library/Application\ Support/Karabiner/private.xml /Users/wmmc/Library/Application\ Support/Karabiner/private.xml-OLD
+ln -s /Users/wmmc/dotfiles/applications/karabiner/private.xml /Users/wmmc/Library/Application\ Support/Karabiner/private.xml
